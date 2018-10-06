96100e0e24bc5d7389344a8b665b70c684f0cd85
Marc Muta <jimmy.mkude@gmail.com>
2018-10-05 20:25:34 -0400

    First Commit, Do not push to this branch at all unless accepted to be on release

---
+++ PortTracker/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Track you Portfolio in the crypto currency market. Project initiative by Sky, Eric and Arnold.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Coin()
+        {
+            ViewBag.Message = "Your Single Coin Page.";
+
+            return View();
+        }
+
+        public ActionResult Coins()
+        {
+            ViewBag.Message = "Your Coins Page.";
+
+            return View();
+        }
+    }
+}
